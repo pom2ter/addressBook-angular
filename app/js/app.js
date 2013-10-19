55ff29acb6761f2ff3de041614363b047c3b89d7
pom2ter <benoit.roy@gmail.com>
2013-10-19 18:23:38 -0400

    ajout de champs

--- app/js/app.js
+++ app/js/app.js
@@ -11,10 +11,10 @@ addressBookApp.config(function($routeProvider) {
 
 addressBookApp.factory('listeContact', function() {
 	var contacts = [
-		{firstName:'Benoit', lastName:'Roy', city:'Montréal'},
-		{firstName:'Isabel', lastName:'Perez', city:'Lima'},
-		{firstName:'Marie-Pier', lastName:'Ramsay', city:'Québec'},
-		{firstName:'Émilie', lastName:'Haché', city:"Val-d'or"}
+		{firstName:'Benoit', lastName:'Roy', email:'uneadresse@uneadresse.com', address:'Montréal', phone:'514-555-1234'},
+		{firstName:'Isabel', lastName:'Perez', email:'uneautreadresse@uneautreadresse.com', address:'Lima', phone:'514-123-4567'},
+		{firstName:'Marie-Pier', lastName:'Ramsay', email:'', address:'Québec', phone:'438-111-2222'},
+		{firstName:'Émilie', lastName:'Haché', email:'', address:"Val-d'or", phone:'819-888-8888'},
 	];
 	var factory = {};
 	factory.getContacts = function() {
@@ -24,21 +24,28 @@ addressBookApp.factory('listeContact', function() {
 });
 
 addressBookApp.controller('AddContactCtrl', function($scope, listeContact) {
+	$scope.liste = listeContact.getContacts();
 	$scope.addContact = function() {
-		$scope.liste.push({firstName:$scope.newContact.firstName, lastName:$scope.newContact.lastName, city:$scope.newContact.city});
+		$scope.liste.push({firstName:$scope.newContact.firstName, lastName:$scope.newContact.lastName, email:$scope.newContact.email, address:$scope.newContact.address, phone:$scope.newContact.phone});
+		window.location = '#/viewAll';
 	}
 });
 
 addressBookApp.controller('EditContactCtrl', function($scope, listeContact) {
-	$scope.editContact = function() {
-		$scope.liste.push({firstName:$scope.newContact.firstName, lastName:$scope.newContact.lastName, city:$scope.newContact.city});
+	$scope.liste = listeContact.getContacts();
+	$scope.editContact = function(item) {
+		var index = $scope.liste.indexOf(item);
+		$scope.liste[index] = {firstName:$scope.editContact.firstName, lastName:$scope.editContact.lastName, email:$scope.editContact.email, address:$scope.editContact.address, phone:$scope.editContact.phone};
 	}
 });
 
 addressBookApp.controller('ViewAllContactsCtrl', function($scope, listeContact) {
 	$scope.liste = listeContact.getContacts();
+	$scope.editContact = function(item) {
+		window.location = '#/edit?item=' + $scope.liste.indexOf(item);
+	}
 	$scope.removeContact = function(item) {
-		var index = $scope.liste.indexOf(item)
+		var index = $scope.liste.indexOf(item);
 		$scope.liste.splice(index, 1);
 	}
 });
